2ad5df9d13a63081b685e74fbde39acd0a2bd891
devesh <deveshgupta3740@gmail.com>
2024-10-06 20:02:30 +0530

    Completed API Part

--- Section 28 (API)/5.5 REST APIs/solution.js
+++ Section 28 (API)/5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "b2b35662-dc95-4f2a-8ee4-dde32cc6af3e";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
